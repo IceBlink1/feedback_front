d1ab489c3040b8c73ba4fe8b0bf293a18ae7cb1c
Alexander Lyutikov <aslyutikov@yandex-team.ru>
2022-02-05 19:23:31 +0300

    Fixed ip

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Main method="POST" action="http://localhost:8000/post/"/>
+                <Main method="POST" action="http://37.46.129.242:8080/post/"/>
             </header>
         </div>
     );
